da71eaa5f5e2b242556fb2bf9961a3aebcea8175
«Vladimir <vjk1976@ukr.net>
2019-03-11 22:50:34 +0200

    elsif

---
+++ elseif.rb
@@ -0,0 +1,23 @@
+puts "Write down percentage of fat:"
+i=gets.to_i
+  if i==15
+    puts "it is sowercreame"
+  elsif i==1
+    puts "it is kefir"
+  elsif i ==70
+    puts "it is butter"
+  else
+    puts "it is milk product"
+  end
+
+
+puts " what is the result of multiplying 3x3 ?"
+i=gets.to_i
+if i==6
+  puts "it is 3+3"
+elsif i==9
+  puts "it is right"
+elsif i==12
+  puts "it is 3*4"
+  else "you answer is wrong"
+end
